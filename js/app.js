ee33f1ce8fa88314e3c6ba8725fe99ba3cdeb4a5
B V Satyaram <bvsatyaram@gmail.com>
2015-05-11 19:53:23 +0530

    Adding Parse

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
+Parse.initialize("qds2VdwPg4RqeS6s3UxQKegf10cWJ05k5d1AIW6Q", "fO92uP1Gd2Luu9gOuXLR1pa3DhBvG67ZJPDDUP8B");
+    
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+  testObject.save({foo: "bar"}, {
+  success: function(object) {
+    $(".success").show();
+  },
+  error: function(model, error) {
+    $(".error").show();
+  }
+});
+
 var app = angular.module('myList', ['ngAnimate']);
 
 app.config(function($sceProvider) {
-    $sceProvider.enabled(false);
-  });
+  $sceProvider.enabled(false);
+});
 
 app.controller('ItemController', ['$scope', function($scope) {
   $scope.items = [
